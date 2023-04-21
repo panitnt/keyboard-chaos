2701bdb0b62ab169b0adb6a79c7e014fdd8b2ffe
natekrth <natekrth@gmail.com>
2023-04-21 22:33:11 +0700

    add out/ in gitignore and add game panel in game

---
+++ src/Game.java
@@ -0,0 +1,25 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class Game extends JFrame {
+    private GamePanel gamePanel = new GamePanel();
+
+    public Game() {
+        add(gamePanel);
+        pack();
+        setVisible(true);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+    }
+
+    class GamePanel extends JPanel {
+        public GamePanel() {
+            setPreferredSize(new Dimension(1600, 800));
+        }
+    }
+
+    public static void main(String[] args) {
+        Game game = new Game();
+    }
+}
+
+
