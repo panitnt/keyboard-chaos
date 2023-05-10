69bffab9dd009698c0424dc82b1ffeb25b90b9cb
panitnt <panittatanya@gmail.com>
2023-05-11 00:46:16 +0700

    Edit Game and GameUI

--- src/Game.java
+++ src/Game.java
@@ -1,11 +1,28 @@
-public class Game {
-    private Word word;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Observable;
+
+public class Game extends Observable {
+    private WordController wordController;
     private Setting setting;
+    private Thread thread;
+    private List<WordController> word_generate= new ArrayList<WordController>();
 
     public Game(){
-        word = new Word();
         setting = Setting.getInstance();
+        initWordController();
+    }
+        public void initWordController(){
+        word_generate.add(new WordController("hello"));
+        word_generate.add(new WordController(" "));
+        word_generate.add(new WordController("world"));
     }
     public void start(){
+        thread = new Thread(){
+            @Override
+            public void run(){
+
+            }
+        };
     };
 }
--- src/GameUI.java
+++ src/GameUI.java
@@ -3,28 +3,42 @@ import javax.swing.border.EmptyBorder;
 import java.awt.*;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
+import java.util.Observable;
+import java.util.Observer;
 
-public class GameUI extends JFrame {
+public class GameUI extends JFrame implements Observer {
     private GamePanel gamePanel;
     private WordPanel wordPanel;
+    private Game game;
+
     public GameUI() {
         gamePanel = new GamePanel();
         wordPanel = new WordPanel();
+        game = new Game();
 
 //        gamePanel.add(wordPanel);
         add(gamePanel);
+        add(wordPanel);
         pack();
-//        setLocationRelativeTo(null);
+        setLocationRelativeTo(null);
+        setSize(900, 600);
         setVisible(true);
+        setAlwaysOnTop(true);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
     }
 
+    @Override
+    public void update(Observable o, Object arg) {
+        wordPanel.repaint();
+
+    }
+
     class GamePanel extends JPanel {
-        private final int PADDING = 250;
+//        private final int PADDING = 50;
 
         public GamePanel() {
-            setBackground(Color.GRAY);
-            setBorder(new EmptyBorder(PADDING, PADDING, PADDING, PADDING));
+            setBackground(Color.LIGHT_GRAY);
+//            setBorder(new EmptyBorder(PADDING, PADDING, PADDING, PADDING));
             addKeyListener(new KeyAdapter() {
                 @Override
                 public void keyPressed(KeyEvent e) {
@@ -35,13 +49,65 @@ public class GameUI extends JFrame {
             });
             setFocusable(true);
         }
+        @Override
+        public void paint(Graphics g) {
+            super.paint(g);
+        }
     }
 
-    class WordPanel extends JPanel{
-        private final int FONT_SIZE = 20;
-        public WordPanel(){
-            setPreferredSize(new Dimension(getWidth()*FONT_SIZE, FONT_SIZE));
+    class WordPanel extends JPanel {
+        private final int FONT_SIZE = 30;
+
+        public WordPanel() {
+            setPreferredSize(new Dimension(getWidth() * FONT_SIZE, FONT_SIZE));
             setBackground(Color.pink);
+        }
+
+        @Override
+        public void paint(Graphics g) {
+            super.paint(g);
+//            Command h = new Command('H');
+//            h.setCorrect(true);
+//            paintWord(g, h, 1, 1);
+//            Command e = new Command('E');
+//            e.setQueue(true);
+//            paintWord(g, e, 2, 1);
+//            paintWord(g, new Command('L'), 3, 1);
+//            paintWord(g, new Command('L'), 4, 1);
+//            paintWord(g, new Command('O'), 5, 1);
+//            paintWord(g, new Command(' '), 6, 1);
+//            paintWord(g, new Command('W'), 7, 1);
+//            paintWord(g, new Command('O'), 8, 1);
+//            paintWord(g, new Command('R'), 9, 1);
+//            paintWord(g, new Command('L'), 10, 1);
+//            paintWord(g, new Command('D'), 11, 1);
+
+
+            for(int row = 0; row < 40; row++){
+                for(int col = 0; col <20; col++){
+                    paintWord(g, new Command('H'), row, col);
+//                    paintWord(g, new Command('E'), row, col);
+//                    paintWord(g, new Command('L'), row, col);
+//                    paintWord(g, new Command('L'), row, col);
+                }
+            }
+
+        }
+
+        private void paintWord(Graphics g, Command c, int x, int y) {
+            g.setColor(Color.WHITE);
+            g.fillRect(x * (this.FONT_SIZE-10), y * this.FONT_SIZE, this.FONT_SIZE-10, this.FONT_SIZE);
+            if (c.isCorrect()) {
+                g.setColor(Color.BLACK);
+            } else if (c.isQueue() && (!c.isCorrect())) {
+                g.setColor(Color.RED);
+            } else {
+
+                g.setColor(Color.LIGHT_GRAY);
+            }
+            g.setFont(new Font("Monospaced", Font.PLAIN, 30));
+            g.drawString(c.getCharacter() + "", (x * (this.FONT_SIZE-10)) + 1, (y * this.FONT_SIZE) + 26);
+
         }
     }
 
